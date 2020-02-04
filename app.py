e949ad7f8e149637a4862ea7d82c2f790624d965
DylanAtRender <59622000+DylanAtRender@users.noreply.github.com>
2020-02-03 18:08:55 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def test_page():
 
 @app.route('/b')
 def button():
-    return '<a href="https://slack.com/oauth/v2/authorize?client_id=331836816675.936989785591&scope=incoming-webhook"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"></a>'
+    return '<a href="https://slack.com/oauth/authorize?client_id=331836816675.936989785591&scope=incoming-webhook"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"></a>'
